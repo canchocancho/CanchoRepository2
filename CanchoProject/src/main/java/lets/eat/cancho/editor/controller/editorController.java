23686b46ae0ecbdd52a3758f706c5cd03434b667
KaneYoon <anum91@naver.com>
2018-04-05 15:16:12 +0900

    윤재호 0405

--- CanchoProject/src/main/java/lets/eat/cancho/editor/controller/editorController.java
+++ CanchoProject/src/main/java/lets/eat/cancho/editor/controller/editorController.java
@@ -1,5 +1,14 @@
 package lets.eat.cancho.editor.controller;
 
-public class editorController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+public class editorController {
+	
+	@RequestMapping(value="videoEditor", method=RequestMethod.GET)
+	public String editor(){
+		return "v_editor";
+	}
 }
