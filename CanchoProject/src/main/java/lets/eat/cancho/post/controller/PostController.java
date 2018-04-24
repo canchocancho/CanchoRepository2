518fb690a86c0d848e4944f63bb1652a29664703
SCITMASTER <SCITMASTER@LAPTOP-S71HL543>
2018-04-24 20:17:17 +0900

    Davin 초대

--- CanchoProject/src/main/java/lets/eat/cancho/post/controller/PostController.java
+++ CanchoProject/src/main/java/lets/eat/cancho/post/controller/PostController.java
@@ -28,7 +28,6 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
