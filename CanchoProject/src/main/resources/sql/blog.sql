a27f946c1df9fdba3f6bfaa1a1a6766f6792b439
SCITMASTER <SCITMASTER@LAPTOP-S71HL543>
2018-04-30 16:42:45 +0900

    sql문 수정 --davin

--- CanchoProject/src/main/resources/sql/blog.sql
+++ CanchoProject/src/main/resources/sql/blog.sql
@@ -1,27 +1,27 @@
 -- blog_user table
 CREATE TABLE blog_user
 (
-    user_id          VARCHAR2(45)     PRIMARY KEY, 
-    user_name        VARCHAR2(45)     NOT NULL, 
-    user_email       VARCHAR2(100)    NOT NULL unique, 
-    user_password    VARCHAR2(100)    NOT NULL,
-    user_verify		 VARCHAR2(50)	  default 'N',
-    user_deleted	 VARCHAR2(50)	  default 'N'
+    user_id          VARCHAR2(45)     	PRIMARY KEY, 
+    user_name        VARCHAR2(45)		NOT NULL, 
+    user_email       VARCHAR2(100)    	NOT NULL unique, 
+    user_password    VARCHAR2(100)    	NOT NULL,
+    user_verify		 VARCHAR2(50)	  	default 'N',
+    user_deleted	 VARCHAR2(50)	  	default 'N'
 );
 
 -- blog_post table
 CREATE TABLE blog_post
 (
-    post_num            NUMBER              PRIMARY KEY, 
-    post_title          VARCHAR2(144)    NOT NULL, 
-    post_title_clean    VARCHAR2(10)     NULL, 
-    post_file           VARCHAR2(300)    NOT NULL, 
-    user_id             VARCHAR2(45)     NOT NULL, 
+    post_num            NUMBER           	PRIMARY KEY, 
+    post_title          VARCHAR2(144)		NOT NULL, 
+    post_title_clean    VARCHAR2(10)     	NULL, 
+    post_file           VARCHAR2(300)    	NOT NULL, 
+    user_id             VARCHAR2(45)     	NOT NULL, 
     originalfile		VARCHAR2(200),
 	savedfile			VARCHAR2(100),
-    post_date           DATE             default SYSDATE,
-    post_like			NUMBER 			 default 0,
-    post_dislike		NUMBER   			 default 0,
+    post_date           DATE            	default SYSDATE,
+    post_like			NUMBER 				default 0,
+    post_dislike		NUMBER   		 	default 0,
     CONSTRAINT blog_p_fk FOREIGN KEY (user_id) REFERENCES blog_user (user_id)
 );
 
@@ -33,8 +33,8 @@ INCREMENT BY 1;
 -- blog_friend table
 CREATE TABLE blog_friend
 (
-    user_id      VARCHAR2(45)    NOT NULL, 
-    friend_id    VARCHAR2(45)    NOT NULL,
+    user_id      VARCHAR2(45)		NOT NULL, 
+    friend_id    VARCHAR2(45)		NOT NULL,
     CONSTRAINT blog_f_fk FOREIGN KEY (user_id) REFERENCES blog_user (user_id),
 	CONSTRAINT blog_f_fk2 FOREIGN KEY (friend_id) REFERENCES blog_user (user_id)
 );
@@ -44,9 +44,9 @@ CREATE TABLE blog_comment
 (
     comment_num     NUMBER              PRIMARY KEY, 
     post_num        NUMBER              NOT NULL, 
-    comment_text    VARCHAR2(300)    NOT NULL, 
-    user_id         VARCHAR2(45)     NOT NULL, 
-    comment_date    DATE             NOT NULL, 
+    comment_text    VARCHAR2(300)    	NOT NULL, 
+    user_id         VARCHAR2(45)     	NOT NULL, 
+    comment_date    DATE             	NOT NULL, 
 	CONSTRAINT blog_c_fk FOREIGN KEY (post_num) REFERENCES blog_post (post_num),
     CONSTRAINT blog_c_fk2 FOREIGN KEY (user_id) REFERENCES blog_user (user_id)
 );
@@ -61,10 +61,10 @@ CREATE TABLE blog_profile
 (
     user_id				VARCHAR2(45),
     user_email       	VARCHAR2(100),
-    p_birthDate   		VARCHAR2(40) NOT NULL,
-    p_sex				VARCHAR2(20) NOT NULL,
-    p_city     			VARCHAR2(100) NOT NULL,
-    p_country  			VARCHAR2(100) NOT NULL,
+    p_birthDate   		VARCHAR2(40)		NOT NULL,
+    p_sex				VARCHAR2(20)		NOT NULL,
+    p_city     			VARCHAR2(100)		NOT NULL,
+    p_country  			VARCHAR2(100)		NOT NULL,
     p_company   		VARCHAR2(100),
     p_school			VARCHAR2(100),
     p_introduce    		VARCHAR2(1000),
